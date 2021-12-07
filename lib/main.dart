004d4c3887a7d1662b6db92471628ea2a92dafe6
Nitael <nitael@okaydev.com.br>
2021-12-06 22:52:08 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/shared/Home/home_page.dart';
+import 'package:payflow/modules/shared/splash/splash_page.dart';
+import 'package:payflow/modules/shared/login/login_page.dart';
+import 'package:payflow/modules/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
